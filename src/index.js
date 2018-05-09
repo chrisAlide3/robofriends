43becb380b62519cf7aef7afa2f3f7001990886b
chris <christian.bracher@gmail.com>
2018-05-09 10:30:38 +0800

    added new Hello component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greetings={'React Ninja'} />, document.getElementById('root'));
 registerServiceWorker();
